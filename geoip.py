4a8a30d7661472e18c9809c0e6f68f3e67389d93
zxryo <37045706+zxryo@users.noreply.github.com>
2020-05-03 20:18:42 +0800

    thread-safe singleton

--- geoip.py
+++ geoip.py
@@ -1,32 +1,25 @@
+from threading import Lock
+
 import geoip2.database
 
 from util import resource_path
 
 
-class Singleton(type):
-    def __init__(cls, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        cls.__INSTANCE = None
+class SingletonMeta(type):
+    _instance = None
+    _lock = Lock()
 
     def __call__(cls, *args, **kwargs):
-        if cls.__INSTANCE is None:
-            cls.__INSTANCE = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls.__INSTANCE
+        with cls._lock:
+            if cls._instance is None:
+                cls._instance = super(SingletonMeta, cls).__call__(*args, **kwargs)
+            return cls._instance
 
 
-class GeoIP(metaclass=Singleton):
+class GeoIP(metaclass=SingletonMeta):
     def __init__(self):
         self.reader = geoip2.database.Reader(resource_path('GeoLite2-Country.mmdb'))
 
     def country(self, ip: str) -> str:
         data = self.reader.country(ip)
         return data.country.iso_code
-
-
-if __name__ == '__main__':
-    t1 = GeoIP()
-    t2 = GeoIP()
-
-    print(id(t1), id(t2))
-    print(t1.country('223.5.5.5'))
-    print(t2.country('8.8.8.8'))
